82c272c235bbe5e1923388a9373eb1227dd7b454
rose Njambilo <rosenjah@gmail.com>
2022-07-23 18:51:28 -0900

    styles update

--- assessment-4/server/index.js
+++ assessment-4/server/index.js
@@ -7,12 +7,18 @@ app.use(cors());
 
 app.use(express.json());
 
-const { getCompliment } = require('./controller');
-const { getCookie } = require('./controller');
-const { getPassword } = require('./controller');
+const {
+  getCompliment,
+  getCookie,
+  getPassword,
+  getRestaurant,
+  postRestaurant,
+} = require('./controller');
 
 app.get("/api/compliment", getCompliment);
 app.get("/api/cookie", getCookie);
 app.get("/api/password", getPassword);
+app.get("/api/restaurant",  getRestaurant);
+app.post("/api/restaurant",  postRestaurant);
 
 app.listen(4000, () => console.log("Server running on 4000"));
